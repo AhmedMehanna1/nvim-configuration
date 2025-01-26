9c5943ac15e982e8cb57dd43585f0cd756cd81b1
Ahmed Mehanna <ahmed.mehanna@hypercell.net>
2025-01-26 12:40:36 +0200

    fix issue in lazy installation

--- lua/mehanna/lazy.lua
+++ lua/mehanna/lazy.lua
@@ -4,7 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy,nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
   })
@@ -12,13 +12,13 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("mehanna.plugins", {
-  --checker = {
-  --  enabled = true,
-  --  notify = false,
-  --},
-  --change_detection = {
-  --  notify = false,
-  --},
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
 
 vim.keymap.set("n", "<leader>lh", "<cmd>Lazy<cr>", { desc = "Open up Lazy plugin manager" })
